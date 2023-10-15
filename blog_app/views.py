fa75c1ceec2f0ad8d2caf81737392f38d9769bbf
satheesh9944 <ssatheesh9944@gmail.com>
2023-10-15 15:36:28 +0530

    post by category

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+from . models import Blog,Category
 
 # Create your views here.
+
+def posts_by_category(request,category_id):
+    posts = Blog.objects.filter(category=category_id,status='Published')
+    category = get_object_or_404(Category,id=category_id)
+    context ={
+        'posts':posts,
+        'category':category
+    }
+    return render(request,'posts_by_category.html',context)
